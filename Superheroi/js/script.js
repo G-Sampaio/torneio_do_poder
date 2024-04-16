a8964468e119322e4a91ed1a6a019ca5808170bd
Guilherme Sampaio <guicwbforever@gmail.com>
2024-04-16 20:39:04 -0300

    SuperHero
    top

---
+++ Superheroi/js/script.js
@@ -0,0 +1,34 @@
+function carregarHerois(){
+
+    const requisicaoHttp = new XMLHttpRequest()
+    requisicaoHttp.open("GET","https://akabab.github.io/superhero-api/api/all.json",false)
+    requisicaoHttp.send()
+    
+    const herois = JSON.parse(requisicaoHttp.responseText)
+
+    for( let index = 0 ; index < herois.length; index ++){
+        const heroi = herois[index]
+
+        adicionarCardHeroi(heroi)
+    }
+}
+
+
+function adicionarCardHeroi(heroi){
+
+    const divCardElemento = document.createElement("div")
+    divCardElemento.classList.add("card")
+
+    const imagemElemento = document.createElement("img")
+    imagemElemento.setAttribute("src", heroi.images.lg)
+    imagemElemento.style.width="90%"
+
+    const textoElemento = document.createElement("h1")
+    textoElemento.innerHTML = heroi.name
+
+    const divElemento = document.getElementById("conteudo-heroi")
+    divCardElemento.appendChild(imagemElemento)
+    divCardElemento.appendChild(textoElemento)
+    divElemento.appendChild(divCardElemento)
+
+}
