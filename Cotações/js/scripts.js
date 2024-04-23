229ea0ae896c55d36a69c7a673a9667e57ccec46
Guilherme Sampaio <guicwbforever@gmail.com>
2024-04-23 20:37:34 -0300

    Cotacao

---
+++ Cotações/js/scripts.js
@@ -0,0 +1,33 @@
+function carregarCotacao() {
+    let url = "https://economia.awesomeapi.com.br/last/USD-BRL,BTC-USD,BTC-BRL,ETH-BRL";
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            MostraCotacao(data);
+        })
+}
+
+function mostrarCotacao(dados) {
+    let conteudoCotacao = document.querySelector('#conteudo-cotacao');
+    for (let moeda in dados) {
+            conteudoCotacao.innerHTML +=
+                "<p>Nome Conversão: " + dados[moeda].name + "</p>" +
+                "<p>Valor: " + dados[moeda].bid + "</p>"
+    }
+}
+
+
+
+// Método sem looping, precisa colocar todas as conversões.
+
+
+// function MostraCotacao(dados){
+//     let conteudoCotacao = document.querySelector('#conteudo-cotacao');
+//     conteudoCotacao.innerHTML = "<p>Nome Conversão: " + dados.USDBRL.name + "</p>" +
+//                                 "<p>Valor: " + dados.USDBRL.bid + "</p>"
+
+
+// }
