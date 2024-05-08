23fc4d1e2bc593af356fcccdf20cfc48e189228e
Guilherme Sampaio <guicwbforever@gmail.com>
2024-05-07 21:31:31 -0300

    Nova-versão

--- Aula 05 - Projeto PokeDex/js/scripts.js
+++ Aula 05 - Projeto PokeDex/js/scripts.js
@@ -1,43 +1,55 @@
+const grupoGrama = ["bulbasaur","venusaur","ivysaur"]
+const grupoFogo = ["charmander","charmeleon","charizard"]
 
-function carregar_pokemons() {
-    let url = "https://pokeapi.co/api/v2/pokemon?limit=100";
-    fetch(url)
-        .then(function(response) {
-            return response.json();
+
+function onPageLoad(){
+
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=1000")
+    .then(function response(data){
+
+        const promiseData = data.json()
+        console.log(promiseData)
+
+        promiseData.then(response =>{
+            insertUsers(response.results)
         })
-        .then(function(data) {
-            console.log(data);
-            data.results.forEach(function(pokemon) {
-                carregarDetalhes(pokemon.url);
-            });
-        })
+ 
+    })
+    
+}
+function insertUsers(pokemons){
+    for( let index = 0 ; index < pokemons.length; index ++){
+        const pokemon = pokemons[index]
+        const id = index + 1
+        pokemon.imagem = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + id +".png"
+        adicionarCardPokemon(pokemon)
+    }
 }
 
-function carregarDetalhes(url) {
-    fetch(url)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(pokemon) {
-            adicionarCardPokemon(pokemon);
-        })
+
+function adicionarCardPokemon(pokemon){
+
+    const divCardElemento = document.createElement("div")
+    divCardElemento.classList.add("card")
+
+    const imagemElemento = document.createElement("img")
+    imagemElemento.setAttribute("src", pokemon.imagem)
+    imagemElemento.style.width="90%"
+
+    const textoElemento = document.createElement("h1")
+    textoElemento.innerHTML = pokemon.name
+
+    if(grupoGrama.includes(pokemon.name)){
+        textoElemento.classList.add("grama")
+    } else if(grupoFogo.includes(pokemon.name)){
+        textoElemento.classList.add("fogo")
+    }
+   
+    const divElemento = document.getElementById("conteudo-pokemon")
+
+    divCardElemento.appendChild(imagemElemento)
+    divCardElemento.appendChild(textoElemento)
+
+    divElemento.appendChild(divCardElemento)
+
 }
-
-function adicionarCardPokemon(pokemon) {
-    const divCardElemento = document.createElement("div");
-    divCardElemento.classList.add("card");
-
-    const imagemElemento = document.createElement("img");
-    imagemElemento.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`);
-    imagemElemento.style.width = "90%";
-
-    const textoElemento = document.createElement("h1");
-    textoElemento.innerHTML = pokemon.name;
-
-    const divElemento = document.getElementById("conteudo-pokemon");
-
-    divCardElemento.appendChild(imagemElemento);
-    divCardElemento.appendChild(textoElemento);
-    divElemento.appendChild(divCardElemento);
-}
-carregar_pokemons();
