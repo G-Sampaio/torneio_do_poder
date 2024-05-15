7b28fddce5e07f3e01722447c43b57d12a54a4fd
Guilherme Sampaio <guicwbforever@gmail.com>
2024-05-14 21:36:49 -0300

    Post-delete

---
+++ AULA 10 - 14.05/js/script.js
@@ -0,0 +1,51 @@
+function savePost(){
+    const bodyPost = getBodyPost()
+    fetch("https://jsonplaceholder.typicode.com/posts",{
+        method : "POST",
+        body : bodyPost
+
+
+})
+.then(data => data.json())
+.then(response => console.log(response))   
+}
+
+
+function updatePost(){
+    const bodyPost = getBodyPost()
+    fetch("https://jsonplaceholder.typicode.com/posts/100",{
+        method : "PUT",
+        body : bodyPost
+
+
+})
+.then(data => data.json())
+.then(response => console.log(response))
+}
+
+
+function deletePost(){
+    fetch("https://jsonplaceholder.typicode.com/posts/100",{
+        method : "DELETE"
+
+
+})
+.then(data => data.json())
+.then(response => console.log(response))
+}
+
+
+function getBodyPost(){
+    const inputTitle =  document.getElementById("input-title").value
+    const inputBody = document.getElementById("input-body").value
+    const inputUserid =  document.getElementById("input-user-id").value
+    const body = JSON.stringify({
+        userid : inputUserid,
+        title : inputTitle,
+        body : inputBody
+  
+    })
+
+    return body
+}
+
